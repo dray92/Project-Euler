142c4348b61a43bd1533492a3bb1e1d89d4195f5
Debosmit Ray <dray92@uw.edu>
2016-04-24 14:09:49 -0700

    done

---
+++ problems/Problem29.java
@@ -0,0 +1,19 @@
+package problems;
+
+import java.math.BigInteger;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Problem29 {
+	public static void main(String[] args) {
+		Set<BigInteger> set = new HashSet<BigInteger>();
+		for(int i = 2 ; i <= 100 ; i++) {
+			BigInteger a = BigInteger.valueOf(i);
+			for(int j = 2 ; j <= 100 ; j++) {
+				set.add(a.pow(j));
+			}
+		}
+		System.out.println(set.size());
+	}
+
+}
